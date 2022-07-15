f611443a2a76b3d1bbbfbd45c683a6f61e3a916a
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2022-07-15 21:06:34 +0500

    updated

--- skillancer_ui/src/components/home.js
+++ skillancer_ui/src/components/home.js
@@ -10,6 +10,16 @@ import rectangle22 from "../images/Rectangle 22.png"
 import rectangle23 from "../images/Rectangle 23.png"
 import rectangle24 from "../images/Rectangle 24.png"
 import vector from "../images/Vector.png"
+import vector1 from "../images/vector1.png"
+import vector2 from "../images/Vector2.png"
+import vector3 from "../images/Vector3.png"
+import vector4 from "../images/Vector4.png"
+import slcaknew from "../images/Slack New.png"
+import woo from "../images/WooCommerce.png"
+// import seehowthimgs from "../images/See how we’re rolling things around.jpg"
+import startfromhere from "../images/Start from here.jpg"
+
+// import { Button } from '@mui/material'
 
 
 
@@ -40,10 +50,59 @@ export const Home = () => {
     
     <img  className='vector' src={vector} alt="img" />
 
-    <div className='content'>
+    <div className='contentp'>
     <p>Find great talent, build your business and take your career to the next level.</p>
     </div>
 
+    <div className='workbutton'>
+
+
+
+    <button className='btn_work'>I am looking for a work
+     <img  className='vector1' src={vector1} alt="img" />
+      </button>
+
+
+
+      <button className='btn_talent'>I need a talent
+     <img  className='vector2' src={vector2} alt="img" />
+      </button>
+
+
+
+    <img className="vector3" src={vector3}  alt='img' />
+
+    <img className="startfromhere" src={startfromhere}  alt='img' />
+    
+
+    </div>
+
+    <div className='play_btn'>
+  
+    <button className='playbtn'> <img  className='rectangle34' src={vector4} alt="img" /> </button>
+    
+     <p className='seehow'>See how we’re rolling things around</p>
+
+    </div>
+
+    <p className='trustedby'>Trusted By</p>
+  
+    <div >
+    <img  className='slacknew' src={slcaknew} alt="img" />
+
+    
+    <a className='slack' href='https://slack.com/' target="_blank">Slack</a>
+
+
+    <img  className='woo' src={woo} alt="img" />
+
+    
+    <a className='commerce' href='https://woocommerce.com/' target="_blank">Commerce</a>
+
+    
+    </div>
+
+    
     </div>
   )
 }
